35fbffa638cf0f43a573a44233274c23d9305040
bandurka887 <fuzzy20075@gmail.com>
2026-04-26 23:21:00 +0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        prevHash = {}
+
+        for i,n in enumerate(nums):
+            if target - n in prevHash:
+                return [prevHash[target - n],i]
+            prevHash[n] = i
+        return
